19c1d8e7cf0d9a632d8dbeb493d8b18e693b077d
Michael Goß <mail@michaelgoss.de>
2025-10-11 16:57:23 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  // import svelteLogo from "./assets/svelte.svg";
+  // import viteLogo from "/vite.svg";
+  // import Counter from "./lib/Counter.svelte";
+  import ColorPicker from "./lib/ColorPicker.svelte";
+</script>
+
+<main>
+  <h1>Hue Far Apart</h1>
+  <h2>Measuring the distance between hues, one pixel at a time</h2>
+  <section class="color-pickers">
+    <ColorPicker />
+    <ColorPicker />
+  </section>
+</main>
+
+<style>
+  .color-pickers {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
